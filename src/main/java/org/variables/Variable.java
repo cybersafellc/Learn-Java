ee47656edbd96b1ca12b3aed4a62b92a064b4f0d
myNameIsNewSpirit <rusnandapurnama@gmail.com>
2026-05-11 17:55:55 +0700

    practice variable

--- src/main/java/org/variables/Variable.java
+++ src/main/java/org/variables/Variable.java
@@ -2,6 +2,16 @@ package org.variables;
 
 public class Variable {
     static void main() {
-        S
+        String text = "Hello World";
+        int num = 1;
+        double numF = 1.5;
+        boolean isActive = false;
+        char test = 'A';
+
+        System.out.println(text);
+        System.out.println(num);
+        System.out.println(numF);
+        System.out.println(isActive);
+        System.out.println(test);
     }
 }
